380a7c3521b30833cac8f3deed2dff34079a14c9
Isaac Flores <isaac.a.flores2@gmail.com>
2019-09-20 19:59:01 -0700

    Rendered Bing Map with pins!

---
+++ StravaTest/MapTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StravaTest
+{
+    [TestClass]
+    class MapTest
+    {
+        [TestMethod]
+        public void ExtractCoordinates_TEst()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ StravaVisualizerTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StravaVisualizerTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StravaVisualizerTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8854f6cc-f737-4f11-a69d-05d1ad572bd2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
