1cd22d6b912b20c7cb5bcca8db537815f382de10
Isaac Flores <isaac.a.flores2@gmail.com>
2019-10-17 23:59:34 -0700

    Added data to home page.

--- StravaVisualizerTest/Doubles/TestData.cs
+++ StravaVisualizerTest/Doubles/TestData.cs
@@ -120,5 +120,38 @@ namespace StravaVisualizerTest.Doubles
 
             return summary2;
         }
+
+        public static ICollection<VisualActivity> MonthVisualActivitiesList()
+        {
+            ICollection<VisualActivity> activities = new List<VisualActivity>();        
+
+            for (int i = 0; i < 10; i++)
+            {
+                var summary = new SummaryActivity(startLatlng: new LatLng(), endLatlng: new LatLng(),
+                    type: ActivityType.Crossfit, movingTime: 60, athlete: new MetaAthlete(123), startDate: new DateTime(2019, 09, i + 1), id: i);
+
+                summary.StartLatlng.Add(30.0F);
+                summary.StartLatlng.Add(40.0F);
+                summary.EndLatlng.Add(30.0F);
+                summary.EndLatlng.Add(40.0F);
+
+                activities.Add(new VisualActivity(summary));
+            }
+
+            for (int i = 0; i< 30; i++)
+            {
+                var summary = new SummaryActivity(startLatlng: new LatLng(), endLatlng: new LatLng(),
+                    type: ActivityType.Crossfit, movingTime: 60, athlete: new MetaAthlete(123), startDate: new DateTime(2019, 10, i+1), id: 9 + i);
+                
+                summary.StartLatlng.Add(30.0F);
+                summary.StartLatlng.Add(40.0F);
+                summary.EndLatlng.Add(30.0F);
+                summary.EndLatlng.Add(40.0F);
+
+                activities.Add(new VisualActivity(summary));
+            }
+
+            return activities;
+        }
     }
 }
