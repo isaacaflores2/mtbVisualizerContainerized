be03bc4fd6faf87008439e8b49860628844663dc
Isaac Flores <isaac.a.flores2@gmail.com>
2019-10-27 08:36:14 -0700

    Updated table Dist and Time formatting.

---
+++ StravaVisualizerTest/TableExtensionsTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StravaVisualizer.Models.Activities;
+using StravaVisualizer.Models.MonthSummary;
+
+namespace StravaVisualizerTest
+{
+    [TestClass]
+    public class TableExtensionsTest
+    {
+        [TestMethod]
+        public void Test_ToStringWithOptionalHours_40_Minutes()
+        {
+            double seconds = 2400;
+            TimeSpan timeSpan = TimeSpan.FromSeconds(seconds);
+
+            var result = timeSpan.ToStringWithOptionalHours();
+
+            Assert.AreEqual("40:00", result);
+        }
+
+        [TestMethod]
+        public void Test_ToStringWithOptionalHours_2_Hours_40_Minutes()
+        {
+            double seconds = 9600;
+            TimeSpan timeSpan = TimeSpan.FromSeconds(seconds);
+
+            var result = timeSpan.ToStringWithOptionalHours();
+
+            Assert.AreEqual("2:40:00", result);
+        }
+    }
+}
