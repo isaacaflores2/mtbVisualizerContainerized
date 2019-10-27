c7b4cefbd10e1aa0adc8d438cfb876550afbf8ef
Isaac Flores <isaac.a.flores2@gmail.com>
2019-10-27 10:02:03 -0700

    Added Error Controller

---
+++ StravaVisualizerTest/ErrorControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StravaVisualizer.Controllers;
+
+namespace StravaVisualizerTest
+{
+    [TestClass]
+    public class ErrorControllerTest
+    {
+        [TestMethod]
+        public void Test_Index()
+        {
+            ErrorController controler = new ErrorController();
+
+            var result = controler.Index() as ViewResult;
+
+            Assert.AreEqual("Error", result.ViewName);
+        }
+    }
+}
