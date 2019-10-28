82137e6ba550daca18214d80987b5c319522de46
isaac_thinkpad <isaac@thinkpad>
2019-10-28 13:54:12 -0700

    Added PageNotFound action and view

--- stravaVisualizer/Controllers/ErrorController.cs
+++ stravaVisualizer/Controllers/ErrorController.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
 
 namespace StravaVisualizer.Controllers
 {    
+    [AllowAnonymous]
     public class ErrorController : Controller
-    {                       
+    {        
         public IActionResult Index()
         {                        
+            return View();
+        }
+
+        public IActionResult PageNotFound()
+        {
             return View();
         }
     }
