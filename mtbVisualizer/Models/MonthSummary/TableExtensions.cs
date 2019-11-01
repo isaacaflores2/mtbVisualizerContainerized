cfd7fdf3f5182c325d6d230c62a1ac543e26c5de
isaac_thinkpad <isaac@thinkpad>
2019-11-01 08:43:40 -0700

    Removed tableextensions old comments

--- mtbVisualizer/Models/MonthSummary/TableExtensions.cs
+++ mtbVisualizer/Models/MonthSummary/TableExtensions.cs
@@ -14,29 +14,7 @@ namespace MtbVisualizer.Models.MonthSummary
             string timeSpanText = string.Format(
             timeSpan.TotalHours >= 1 ? @"{0:h\:mm\:ss}" : @"{0:mm\:ss}", timeSpan);
 
-            return timeSpanText;
-            //var stringBuilder = new StringBuilder();
-
-            //if ((int)timeSpan.TotalHours > 0)
-            //{
-            //    stringBuilder.Append((int)timeSpan.TotalHours);
-            //    stringBuilder.Append(":");
-            //}
-
-
-
-            //stringBuilder.Append(timeSpan.Minutes.ToString("d2"));
-            //if (timeSpan.Seconds < 10)
-            //{
-            //    stringBuilder.Append(":0");
-            //}
-            //else
-            //{
-            //    stringBuilder.Append(":");
-            //}
-            //stringBuilder.Append(timeSpan.Seconds.ToString("s"));
-
-            //return stringBuilder.ToString();
+            return timeSpanText;        
         }
     }
 }
