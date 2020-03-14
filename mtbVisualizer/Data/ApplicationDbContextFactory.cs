a6e912c4887cdbe06fa7538b8638114c2d4df70c
isaacaflores2 <isaac.a.flores2@gmail.com>
2020-03-13 17:38:58 -0700

    Added db context

---
+++ mtbVisualizer/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using MtbVisualizer.Data;
+using System;
+
+namespace mtbVisualizer.Data
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            return new ApplicationDbContext(new DbContextOptionsBuilder<ApplicationDbContext>()            
+                .UseSqlServer("Server=localhost,5433;Database=master;User=sa;Password=Pass@word;")
+                .Options);
+        }
+    }
+}
